accf930bf2b6119f6462a6aee3b3ad3255a1be61
mjenadi <62653277+mjenadi@users.noreply.github.com>
2022-01-19 13:37:22 +0100

    Add files via upload

---
+++ Add-StoreApp-Package.ps1
@@ -0,0 +1,5 @@
+﻿$FullPathtoAppxbundle = "YourPathtoAppxBundle" 
+$DependencyFolderPath = "YourDepencyFolderPath" 
+$Dependencies = Get-ChildItem -Path $DependencyFolderPath -Filter ".appx" | Select-Object -ExpandProperty FullName
+
+Add-AppxPackage -Path $FullPathtoAppxbundle -DependencyPath $Dependencies
